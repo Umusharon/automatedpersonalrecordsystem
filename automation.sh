29c20e9241e85cdc6f6dec80940bd75f4c8f5236
Umusharon <sharonumurewa5@gmail.com>
2023-10-05 18:56:44 +0200

    authomate the record

---
+++ automation.sh
@@ -0,0 +1,14 @@
+echo "
+Welcome to Automated Personal Record Management System
+"
+echo "Choose an Option From Menu Below:"
+echo "*********************************"
+echo "
+1. Add new file to Credentials Folder
+2. Remove file from Credentials folder
+3. Rename file from Credentials folder
+4. Change file permission (rwx) from Credentials folder
+5. Quit
+"
+echo "Enter choice:"
+read user_choice
